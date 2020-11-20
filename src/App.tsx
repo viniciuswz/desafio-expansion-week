12c38c73725625b637980e7c9e7b83d534d416ce
Viniciuswz <vinicius-araujo0@hotmail.com>
2020-11-20 15:38:08 -0300

    feature: deploy gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter>
+      <BrowserRouter basename={window.location.pathname || ''}>
         <Routes />
       </BrowserRouter>
     </>
